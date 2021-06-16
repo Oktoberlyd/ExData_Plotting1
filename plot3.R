d694faf089bdc3e08cd5756115453a16434c2279
Oktoberlyd <70793612+Oktoberlyd@users.noreply.github.com>
2021-06-16 12:22:48 +0200

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,67 @@
+##############################################
+#                                            #
+# Exploratory Data Analysis - Week 1 project #
+#                                            #
+#   Plot 3                                   #
+#                                            #
+#                                            #
+##############################################
+
+# libaries are loaded
+library(tidyverse)
+library(lubridate)
+library(data.table)
+
+#working directory is set
+setwd("C:/Users/ocari/OneDrive/Documents/Coursera")
+
+
+#data i collected and unzipped
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, "electric_power.zip")
+unzip("electric_power.zip") #data is unzipped 
+
+#check files
+list.files()
+
+#load and check out files
+household_power_consumption <- fread("household_power_consumption.txt")
+str(household_power_consumption) #formats needs to be changed
+
+#changing date format
+household_power_consumption$Date <- dmy(household_power_consumption$Date)
+
+#only data from dates  2007-02-01 and 2007-02-02
+household_two_dates <- dplyr::filter(household_power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+
+
+#creating date_time variable
+date_time <- paste(household_two_dates$Date, household_two_dates$Time)
+household_two_dates$date_time <- as.POSIXct(date_time)
+
+#changing rest of formats
+household_two_dates$Global_active_power <- as.numeric(household_two_dates$Global_active_power)
+household_two_dates$Global_reactive_power <- as.numeric(household_two_dates$Global_reactive_power)
+household_two_dates$Voltage <- as.numeric(household_two_dates$Voltage)
+household_two_dates$Global_intensity <- as.numeric(household_two_dates$Global_intensity)
+household_two_dates$Sub_metering_1 <- as.numeric(household_two_dates$Sub_metering_1)
+household_two_dates$Sub_metering_2 <- as.numeric(household_two_dates$Sub_metering_2)
+household_two_dates$Sub_metering_3 <- as.numeric(household_two_dates$Sub_metering_3)
+
+#recheck
+str(household_two_dates) #formats look allright
+
+#plot three
+{
+  plot(household_two_dates$Sub_metering_1 ~ household_two_dates$date_time, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(household_two_dates$Sub_metering_2 ~ household_two_dates$date_time,col='Red')
+  lines(household_two_dates$Sub_metering_3 ~ household_two_dates$date_time,col='Blue')
+  legend("topright", col = c("black", "blue", "red"),
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty = 1)
+}
+
+
+# copy plot to a PNG file
+dev.copy (png, file = "plot3.png" )
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,96 @@
+##############################################
+#                                            #
+# Exploratory Data Analysis - Week 1 project #
+#                                            #
+#   Plot 4                                   #
+#                                            #
+#                                            #
+##############################################
+
+# libaries are loaded
+library(tidyverse)
+library(lubridate)
+library(data.table)
+
+#working directory is set
+setwd("C:/Users/ocari/OneDrive/Documents/Coursera")
+
+
+#data i collected and unzipped
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, "electric_power.zip")
+unzip("electric_power.zip") #data is unzipped 
+
+#check files
+list.files()
+
+#load and check out files
+household_power_consumption <- fread("household_power_consumption.txt")
+str(household_power_consumption) #formats needs to be changed
+
+#changing date format
+household_power_consumption$Date <- dmy(household_power_consumption$Date)
+
+#only data from dates  2007-02-01 and 2007-02-02
+household_two_dates <- dplyr::filter(household_power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+
+
+#creating date_time variable
+date_time <- paste(household_two_dates$Date, household_two_dates$Time)
+household_two_dates$date_time <- as.POSIXct(date_time)
+
+#changing rest of formats
+household_two_dates$Global_active_power <- as.numeric(household_two_dates$Global_active_power)
+household_two_dates$Global_reactive_power <- as.numeric(household_two_dates$Global_reactive_power)
+household_two_dates$Voltage <- as.numeric(household_two_dates$Voltage)
+household_two_dates$Global_intensity <- as.numeric(household_two_dates$Global_intensity)
+household_two_dates$Sub_metering_1 <- as.numeric(household_two_dates$Sub_metering_1)
+household_two_dates$Sub_metering_2 <- as.numeric(household_two_dates$Sub_metering_2)
+household_two_dates$Sub_metering_3 <- as.numeric(household_two_dates$Sub_metering_3)
+
+#recheck
+str(household_two_dates) #formats look allright
+
+#Setting 4 graphs
+par(mfrow = c(2, 2))
+
+#Graph one
+plot(household_two_dates$date_time,
+      household_two_dates$Global_active_power,
+      type = "l",
+      xlab = "",
+      ylab = "Global Active Power")
+
+#Graph two
+plot(household_two_dates$date_time,
+     household_two_dates$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage")
+
+#Graph three
+{
+  plot(household_two_dates$Sub_metering_1 ~ household_two_dates$date_time, type="l",
+       ylab="Energy sub metering", xlab="")
+  lines(household_two_dates$Sub_metering_2 ~ household_two_dates$date_time,col='Red')
+  lines(household_two_dates$Sub_metering_3 ~ household_two_dates$date_time,col='Blue')
+  legend("topright", col = c("black", "blue", "red"),
+         legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty = 1)
+}
+
+## Graph four
+     plot(household_two_dates$date_time,
+          household_two_dates$Global_reactive_power,
+          type = "l",
+          xlab = "datetime",
+          ylab = "Global_reactive_power")
+
+
+# copy plot to a PNG file
+dev.copy (png, file = "plot4.png" )
+dev.off()
+
+
+
+
+
